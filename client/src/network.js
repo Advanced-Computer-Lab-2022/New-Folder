745c1a4933132a48dcdd6781ad56686cee4a2bbf
Abdelrahman Fathy <abdof1723@gmail.com>
2022-10-28 16:57:02 +0200

    add comments to network.js

--- client/src/network.js
+++ client/src/network.js
@@ -5,6 +5,7 @@ const MAX_TIMEOUT = 60000;
 
 const baseURI = "http://localhost:8080/";
 
+// Explore
 export const fetchExploreData = async () => {
   const res = await axios.get(baseURI, {
     timeout: MAX_TIMEOUT,
@@ -13,49 +14,43 @@ export const fetchExploreData = async () => {
   return res.data;
 };
 
+
+// Create Course
 export const postCourse = async (data) => {
   const res = await axios.post(`${baseURI}instructor/createCourse`, data);
   console.log(res.data);
   return res.data;
 };
 
-export const postInstructor = async (data) => {
-  const res = await axios.post(`${baseURI}admin/addInstructor`, data);
-  console.log(res.data);
-  return res.data;
-};
-
-export const postCorporateTrainee = async (data) => {
-  const res = await axios.post(`${baseURI}admin/addCorporateTrainee`, data);
-  console.log(res.data);
-  return res.data;
-};
-
+// Add Admin
 export const postAddadmin = async (data) => {
   const res = await axios.post(`${baseURI}admin/addAdmin`, data);
   console.log(res.data);
   return res.data;
 };
 
+// Add Instructor
 export const postAddInstructor = async (data) => {
   const res = await axios.post(`${baseURI}admin/addInstructor`, data);
   console.log(res.data);
   return res.data;
 };
 
-
+// Add Corporate Trainee
 export const postAddCorporateTrainee = async (data) => {
   const res = await axios.post(`${baseURI}admin/addCorporateTrainee`, data);
   console.log(res.data);
   return res.data;
 };
 
+// Search results
 export const fetchSearchData = async (query) => {
   const res = await axios.post(`${baseURI}search`, query);
   console.log(res.data);
   return res.data;
 };
 
+// Login
 export const login = async (loginData) => {
   const res = await axios.post(`${baseURI}login`, loginData);
   return res;
