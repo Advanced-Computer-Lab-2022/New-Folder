63c5752345a4ef618ae1c8c926f985a050a368d0
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-10-29 11:00:37 +0200

    sessionStorage

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Login from "./pages/Login";
 import Navbar from "./components/Navbar/Navbar";
 import { useState } from "react";
 
-ReactSession.setStoreType("localStorage");
+ReactSession.setStoreType("sessionStorage");
 
 function App() {
   const [country, setCountry] = useState("EG");
