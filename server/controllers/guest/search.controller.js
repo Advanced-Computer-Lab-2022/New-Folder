ae5de914e6f2b2316435ba29ada64720789cf31e
Ziad Othman <ziadsadek999@gmail.com>
2022-11-19 11:14:14 +0200

    Update search.controller.js

--- server/controllers/guest/search.controller.js
+++ server/controllers/guest/search.controller.js
@@ -1,4 +1,4 @@
-const Course = require("../models/Course");
+const Course = require("../../models/Course.model");
 
 exports.postSearch = async (req, res) => {
   const courses = await Course.aggregate([
