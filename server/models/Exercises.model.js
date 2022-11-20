5185175dfc8cbb5309b34894c1e19f11bd23ce75
Elsokkary101 <alyhassan123456@gmail.com>
2022-11-20 15:05:46 +0200

    adding reviews.model and adding Mark in Excercises.model

--- server/models/Exercises.model.js
+++ server/models/Exercises.model.js
@@ -4,6 +4,9 @@ const Exercises = mongoose.Schema({
   Questions: {
     type: [mongoose.Schema.Types.Mixed], // [{statement: , choices: , correctIdx}]
   },
+  Mark : {
+    type : [mongoose.Schema.Types.Mixed], // {Trainee_ID , Mark}
+  }
 });
 
 module.exports = mongoose.model("Exercises", Exercises);
---
+++ server/models/Reviews.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Reviews = mongoose.Schema({
+
+  trainee: {
+    type: [{ type: mongoose.Schema.Types.Mixed, ref: "Trainee" }],
+  },
+  review: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Reviews", Reviews);
