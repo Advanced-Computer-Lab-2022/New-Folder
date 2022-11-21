d1a8e3ff6d2018fb62433bcba11d795c0db94dc1
Elsokkary101 <alyhassan123456@gmail.com>
2022-11-21 22:19:48 +0200

    adjusting subtitle array by adding subTitle_content

--- server/models/Insertions.model.js
+++ server/models/Insertions.model.js
@@ -1,12 +1,13 @@
 // don't run the data already on the db
 const mongoose = require("mongoose");
-const Content = require("./Content");
-const Course = require("./Course");
-const Exercises = require("./Exercises");
-const Instructor = require("./Instructor");
-const Subtitle = require("./Subtitle");
-const Trainee = require("./Trainee");
+const Content = require("./Content.model");
+const Course = require("./Course.model");
+const Exercises = require("./Exercises.model");
+const Instructor = require("./Instructor.model");
+const Subtitle = require("./Subtitle.model");
+const Trainee = require("./Trainee.model");
 const bodyParser = require("body-parser");
+const constants = require('../constants.json');
 
 mongoose
   .connect(
@@ -62,70 +63,70 @@ async function insertCourse(
 }
 
 // insert new Instructor
-async function insertInstructor(
-  email,
-  username,
-  password,
-  gender,
-  firstName,
-  lastName,
-  image,
-  country,
-  about,
-  rating,
-  ratingNo,
-  courses,
-  reviews
-) {
-  let c = await Instructor.create({
-    email: email,
-    username: username,
-    password: password,
-    gender: gender,
-    firstName: firstName,
-    lastName: lastName,
-    image: image,
-    country: country,
-    about: about,
-    courses: courses,
-    ratingNo: ratingNo,
-    rating: rating,
-    reviews: reviews,
-  });
+// async function insertInstructor(
+//   email,
+//   username,
+//   password,
+//   gender,
+//   firstName,
+//   lastName,
+//   image,
+//   country,
+//   about,
+//   rating,
+//   ratingNo,
+//   courses,
+//   reviews
+// ) {
+//   let c = await Instructor.create({
+//     email: email,
+//     username: username,
+//     password: password,
+//     gender: gender,
+//     firstName: firstName,
+//     lastName: lastName,
+//     image: image,
+//     country: country,
+//     about: about,
+//     courses: courses,
+//     ratingNo: ratingNo,
+//     rating: rating,
+//     reviews: reviews,
+//   });
 
-  console.log("INSTRUCTOR has been added ✔️");
-  return c;
-}
+//   console.log("INSTRUCTOR has been added ✔️");
+//   return c;
+// }
 
 //insert new Trainee (individual or corporate)
-async function insertTrainee(
-  email,
-  username,
-  password,
-  fields,
-  gender,
-  firstName,
-  lastName,
-  image,
-  country,
-  courses
-) {
-  let c = await Trainee.create({
-    email: email,
-    username: username,
-    password: password,
-    fields: fields,
-    gender: gender,
-    firstName: firstName,
-    lastName: lastName,
-    image: image,
-    country: country,
-    courses: courses,
-  });
+// async function insertTrainee(
+//   email,
+//   username,
+//   password,
+//   fields,
+//   gender,
+//   firstName,
+//   lastName,
+//   image,
+//   country,
+//   courses
+// ) {
+//   let c = await Trainee.create({
+//     email: email,
+//     username: username,
+//     password: password,
+//     fields: fields,
+//     gender: gender,
+//     firstName: firstName,
+//     lastName: lastName,
+//     image: image,
+//     country: country,
+//     courses: courses,
+//   });
 
-  console.log("TRAINEE has been added ✔️");
-  return c;
-}
+//   console.log("TRAINEE has been added ✔️");
+//   return c;
+// }
 
 // insert new content
 async function insertContent(courseID, description, duration, video) {
@@ -165,29 +166,28 @@ async function insertExcercises(
 async function insertSubtitle(subtitleNumber, Contents, exercises) {
   let c = await Subtitle.create({
     subtitleNumber: subtitleNumber,
-    Contents: Contents,
-    exercises: exercises,
+    subTitle_Content : [{subTitle_Content_id  : Contents._id, type : constants.Content} , {subTitle_Content_id  : Contents._id, type : constants.Content} , {subTitle_Content_id  : exercises._id, type : constants.excercise}]
   });
   console.log("Subtitle has been added ✔️");
   return c;
 }
 
 // insert a new trainee to a course
-async function insertTraineeToCourse(courseID, traineeID) {
-  let courseReq = await Course.find({ _id: courseID });
-  courseReq[0].trainees.push(traineeID);
+// async function insertTraineeToCourse(courseID, traineeID) {
+//   let courseReq = await Course.find({ _id: courseID });
+//   courseReq[0].trainees.push(traineeID);
 
-  await Course.updateMany(
-    {
-      _id: courseID,
-    },
-    {
-      trainees: courseReq[0].trainees,
-    }
-  );
+//   await Course.updateMany(
+//     {
+//       _id: courseID,
+//     },
+//     {
+//       trainees: courseReq[0].trainees,
+//     }
+//   );
 
-  console.log("DONE INSERTION of trainee to course");
-}
+//   console.log("DONE INSERTION of trainee to course");
+// }
 
 // insert a new subtitle into this course
 
@@ -208,67 +208,55 @@ async function insertSubtitleToCourse(courseID, subtitle) {
 }
 
 async function populate() {
-  let soubra = await insertInstructor(
-    "soubra@gmail.com",
-    "soubraJokexD",
-    "0000",
-    "Male",
-    "Hassan Hassona",
-    "Soubra",
-    "",
-    "UAE",
-    "RAGL MEYA MEYA",
-    5,
-    5,
-    [],
-    []
-  );
+  // let soubra = await insertInstructor(
+  //   "soubra@gmail.com",
+  //   "soubraJokexD",
+  //   "0000",
+  //   "Male",
+  //   "Hassan Hassona",
+  //   "Soubra",
+  //   "",
+  //   "UAE",
+  //   "RAGL MEYA MEYA",
+  //   5,
+  //   5,
+  //   [],
+  //   []
+  // );
 
   let java = await insertCourse(
-    "Java OOP",
-    "Computer Science",
+    "sczxczxcxzc",
+    "Cxxcxcxcxcxcxce",
     18,
     "USA",
-    "This course introduces computer programming using the JAVA programming language with object-oriented programming principles",
+    "This courasdasdas dasd asd the JAVA programming language with object-oriented programming principles",
     "https://www.aacomputercollege.com/wp-content/uploads/2018/08/java-1030x579.jpg",
     "",
     5,
     5,
     [],
-    soubra._id,
+    "123",
     "Hassan Soubra",
     [],
     []
   );
 
-  let sokk = await insertTrainee(
-    "alyhassan123456@gmail.com",
-    "Elsokkary101",
-    "1234",
-    ["Computer Science"],
-    "Male",
-    "Aly Hassan",
-    "Elsokkary",
-    "",
-    "",
-    [java._id],
-    false
-  );
+
 
   let questionaya = await insertExcercises(
-    "masr gabl el goal el wa7eed fe kas el 3ala sanet kam ?",
-    "1900",
-    "4526",
+    "masr gablmas r masr m,as rmasrrrr rrrrrr kam ?",
+    "19dasdasdasd",
+    "45sasdas6",
     "2001",
     "1920",
     3
   );
   let contentaya = await insertContent(java._id, "masr masr masrrrr", "", "");
 
-  let Subtitlaya = await insertSubtitle(1, [contentaya], [questionaya]);
+  let Subtitlaya = await insertSubtitle(1, contentaya, questionaya);
 
-  await insertTraineeToCourse(java._id, sokk._id);
-  await insertSubtitleToCourse(java._id, Subtitlaya);
+  // await insertTraineeToCourse(java._id, sokk._id);
+  // await insertSubtitleToCourse(java._id, Subtitlaya);
 
   console.log("Relation has been done ✔️ ");
 }
--- server/models/Subtitle.model.js
+++ server/models/Subtitle.model.js
@@ -1,15 +1,30 @@
 const mongoose = require("mongoose");
-
+const SubTitle_Content = require("./SubTitleContent.schema")
+const constants = require("../constants.json");
 const Subtitle = mongoose.Schema({
   subtitleNumber: {
     type: Number,
   },
-  Contents: {
-    type: [{ type: mongoose.Types.ObjectId, ref: "Content" }],
+  // Contents: {
+  //   type: [{ type: mongoose.Types.ObjectId, ref: "Content" }],
+  // },
+  // exercises: {
+  //   type: [{ type: mongoose.Types.ObjectId, ref: "Exercises" }],
+  // },
+  subTitle_Content : {
+    type : [   { subTitle_Content_id : {
+      type : mongoose.Types.ObjectId,
+      required : true
   },
-  exercises: {
-    type: [{ type: mongoose.Types.ObjectId, ref: "Exercises" }],
-  },
+  type : {
+      type  : String,
+      enum : [constants.content , constants.excercise],
+      default : constants.content,
+      required : true
+  }
+}],
+    default:[]
+  }
 });
 
 module.exports = mongoose.model("Subtitle", Subtitle);
