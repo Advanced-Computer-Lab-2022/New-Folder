4c1ce9f3befd062f0258db5df76d3f62bc53c64c
Abdelrahman Fathy <abdof1723@gmail.com>
2022-11-24 18:53:27 +0200

    subtitle card implemented

--- client/src/components/SubtitleCard/SubtitleCard.jsx
+++ client/src/components/SubtitleCard/SubtitleCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { fetchSubtitle } from "../../network";
+import { useNavigate } from "react-router-dom";
 
 function SubtitleCard(props) {
-  const [subtitle, setSubtitle] = useState({});
+  const navigate = useNavigate();
+  const [subtitle, setSubtitle] = useState({ subTitle_Content: [] });
   const getSubtitle = async () => {
     try {
       const fetchedSubtitle = await fetchSubtitle(props.subtitleId);
@@ -17,7 +19,28 @@ function SubtitleCard(props) {
     getSubtitle();
   }, []);
 
-  return <div>{subtitle.subtitleNumber}</div>;
+  return (
+    <>
+      <h5>{"subtitle: " + subtitle.subtitleNumber}</h5>
+      <ol style={{ border: "1px dotted black" }}>
+        {subtitle.subTitle_Content.map((content, index) => {
+          return (
+            <li>
+              <h5>
+                {"content " + (index + 1) + ": "}
+                <a href={"/subtitle/"+props.subtitleId+"?idx="+ index}>
+                  {content.type === "content"
+                    ? "watch video"
+                    : "solve exercise"}
+                </a>
+              </h5>
+              <br />
+            </li>
+          );
+        })}
+      </ol>
+    </>
+  );
 }
 
 export default SubtitleCard;
