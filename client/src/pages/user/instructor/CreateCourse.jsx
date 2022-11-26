27ac3be366e42bc99d2979c3a62b2b2695f51b59
Abdelrahman Fathy <abdof1723@gmail.com>
2022-11-27 00:33:22 +0200

    refactor-CreateCourse Logic

--- client/src/pages/user/instructor/CreateCourse.jsx
+++ client/src/pages/user/instructor/CreateCourse.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { postCourse } from "../../../network";
 import { useNavigate } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import CountryCurrency from "iso-country-currency";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/Row";
 
 const countries = CountryCurrency.getAllISOCodes();
 
@@ -13,7 +17,18 @@ function CreateCourse() {
   const [description, setDescription] = useState("");
   const [magnitude, setMagnitude] = useState("");
   const [currency, setCurrency] = useState("EGP");
-  const [subtitle, setSubtitle] = useState("");
+  const [subtitles, setSubtitles] = useState([""]);
+
+  const removeSubtitle = (index) => {
+    const temp = [...subtitles];
+
+    // removing the element using splice
+    temp.splice(index, 1);
+
+    // updating the list
+    setSubtitles(temp);
+  };
+
   const onSubmit = (e) => {
     const data = {
       name: name,
@@ -21,7 +36,7 @@ function CreateCourse() {
       description: description,
       magnitude: magnitude,
       currency: currency,
-      subtitle: subtitle,
+      subtitles: subtitles,
     };
     postCourse(data);
     e.preventDefault();
@@ -30,72 +45,117 @@ function CreateCourse() {
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          placeholder="Enter your name"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <br></br>
-        <input
-          type="text"
-          id="field"
-          name="field"
-          value={field}
-          placeholder="Enter your field"
-          onChange={(e) => setField(e.target.value)}
-        />
-        <br></br>
-        <input
-          type="text"
-          id="description"
-          name="description"
-          value={description}
-          placeholder="Enter your description"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <br></br>
-        <input
-          type="text"
-          id="subtitle"
-          name="subtitle"
-          value={subtitle}
-          placeholder="Enter course subtitle"
-          onChange={(e) => setSubtitle(e.target.value)}
-        />
-        <br></br>
-        <input
-          type="text"
-          id="magnitude"
-          name="magnitude"
-          value={magnitude}
-          placeholder="Enter your price magnitude"
-          onChange={(e) => setMagnitude(e.target.value)}
-        />
-        <br></br>
-        <Form.Select
-          id="currency"
-          name="currency"
-          onChange={(e) => {
-            console.log(e.target.value);
-            setCurrency(e.target.value);
-          }}
-        >
-          {countries.map((country) => (
-            <option
-              selected={country.currency === currency}
-              value={country.currency}
-            >
-              {country.countryName + " (" + country.currency + ")"}
-            </option>
-          ))}
-        </Form.Select>
-        <br></br>
-        <button type="submit">Submit</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Container className="mt-4">
+          <Col lg="5">
+            <Form.Group className="">
+              <Form.Label>Create Course</Form.Label>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                placeholder="Enter Course Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="text"
+                id="field"
+                name="field"
+                value={field}
+                placeholder="Enter Course Field"
+                onChange={(e) => setField(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                as="textarea"
+                rows={3}
+                id="description"
+                name="description"
+                value={description}
+                placeholder="Enter Course Description"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Form.Group>
+            {subtitles.map((sub, index) => {
+              return (
+                <Row>
+                  <Form.Group className="mb-3" as={Col}>
+                    <Form.Control
+                      type="text"
+                      id={"subtitle" + index}
+                      value={subtitles[index]}
+                      name={"subtitle" + index}
+                      placeholder="Enter subtitle"
+                      onChange={(e) => {
+                        const temp = [...subtitles];
+                        temp[index] = e.target.value;
+                        setSubtitles(temp);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group as={Col}>
+                    <Button
+                      className="btn btn-danger "
+                      onClick={() => removeSubtitle(index)}
+                    >
+                      x
+                    </Button>
+                  </Form.Group>
+                </Row>
+              );
+            })}
+            <Form.Group className="mb-3">
+              <Button
+                onClick={(e) => {
+                  setSubtitles([...subtitles, ""]);
+                }}
+              >
+                Add Subtitle
+              </Button>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Control
+                type="text"
+                id="magnitude"
+                name="magnitude"
+                value={magnitude}
+                placeholder="Enter your price magnitude"
+                onChange={(e) => setMagnitude(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Select
+                id="currency"
+                name="currency"
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  setCurrency(e.target.value);
+                }}
+              >
+                {countries.map((country) => (
+                  <option
+                    selected={country.currency === currency}
+                    value={country.currency}
+                  >
+                    {country.countryName + " (" + country.currency + ")"}
+                  </option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Button variant="dark" type="submit">
+                submit
+              </Button>
+            </Form.Group>
+          </Col>
+        </Container>
+      </Form>
     </div>
   );
 }
--- server/controllers/instructor/createCourse.controller.js
+++ server/controllers/instructor/createCourse.controller.js
@@ -4,21 +4,27 @@ const InstructorModel = require("../../models/Instructor.model");
 const Subtitle = require("../../models/Subtitle.model");
 
 const createCourse = async (req, res) => {
-  const content = await Content.create({
-    description: req.body.subtitle,
-  });
-  const subtitle = await Subtitle.create({
-    subtitle: 1,
-    Contents: [content._id],
-  });
+  // const subtitles = await req.body.subtitles.map(async (sub, index) => {
+  //   const subtitle = await Subtitle.create({
+  //     subtitleNumber: index + 1,
+  //   });
+  //   return subtitle._id;
+  // });
+  const subtitles = [];
+  for (let i = 0; i < req.body.subtitles; i++) {
+    const subtitle = await Subtitle.create({
+      subtitleNumber: index + 1,
+    });
+    subtitles.push(subtitle._id);
+  }
   const course = await Course.create({
     description: req.body.description,
     name: req.body.name,
     field: req.body.field,
     price: { magnitude: req.body.magnitude, currency: req.body.currency },
     instructorInfo: { id: req.session.userId, name: req.session.userName },
-    duration: 10,
-    subtitles: [subtitle._id],
+    duration: 0,
+    subtitles: subtitles,
   });
   await updateInstructorCourses(course._id, req.session.userId);
   res.json(course);
