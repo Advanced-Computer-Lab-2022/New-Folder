7f40ae7112e6918f86a18269eace5933a0e90dd9
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-11-28 22:50:31 +0200

    Update editSubtitle.controller.js

--- server/controllers/instructor/editSubtitle.controller.js
+++ server/controllers/instructor/editSubtitle.controller.js
@@ -1,7 +1,22 @@
 const Content = require("../../models/Content.model");
 const Subtitle = require("../../models/Subtitle.model");
+const constants = require("../constants.json");
 
-const addVideo = async (req, res) => {};
+const addVideo = async (req, res) => {
+  const { courseID, subtitleID, videoURL, duration, description } = req.body;
+  const content = await Content.create({
+    courseID,
+    description,
+    duration,
+    video: videoURL,
+  });
+  const subtitle = await Subtitle.findById(subtitleID);
+  subtitle.subTitle_Content.push({
+    subTitle_Content_id: content._id,
+    type: constants.content,
+  });
+  res.status(200).json();
+};
 
 const createExam = async (req, res) => {};
 
