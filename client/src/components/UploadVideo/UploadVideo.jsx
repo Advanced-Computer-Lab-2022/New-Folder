ef167b6d0725f8c8e8c5455de19b9ff05dc8a4ff
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-11-28 21:43:10 +0200

    Update UploadVideo.jsx

--- client/src/components/UploadVideo/UploadVideo.jsx
+++ client/src/components/UploadVideo/UploadVideo.jsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const UploadVideo = (props) => {
   const subtitleID = props.subtitleID;
 
-  return <div></div>;
+  const [videoURL, setVideoURL] = useState("");
+  const [description, setDescription] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    const videoContent = {
+      videoURL,
+      description,
+    };
+    try {
+      // add content
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={submit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Video url</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setVideoURL(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Add
+      </Button>
+    </Form>
+  );
 };
 
 export default UploadVideo;
