3fbdeef168122d8fc20022075c27bc2469f07ff2
Ziad Othman <ziadsadek999@gmail.com>
2022-11-29 06:50:27 +0200

    Middleware bug fixed

--- server/middlewares/enrolledTraineeMiddleware.js
+++ server/middlewares/enrolledTraineeMiddleware.js
@@ -3,6 +3,7 @@ const constants = require("../constants.json");
 const viewerContexts = require("../viewer-contexts.json");
 const { getVC } = require("../utils/getViewerContext.util");
 const Course = require("../models/Course.model");
+const Subtitle = require("../models/Subtitle.model");
 
 const canRateCourse = asyncHandler(async (req, res, next) => {
   if (!req.session.userId) {
@@ -51,22 +52,7 @@ const canDeleteRating = asyncHandler(async (req, res, next) => {
     throw new Error("You have not rated this course before");
   }
 });
-const isEnrolled = asyncHandler(async (req, res, next) => {
-  if (!req.session.userId) {
-    throw new Error("you are not logged in");
-  }
-  const vc = await getVC(
-    req.session.userId,
-    req.session.userType,
-    req.body.courseId
-  );
-  if (vc !== viewerContexts.enrolledTrainee) {
-    throw new Error("You are not enrolled in this course");
-  }
-  next();
-});
 module.exports = {
   canRateCourse,
   canDeleteRating,
-  isEnrolled,
 };
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -15,10 +15,10 @@ const {
   updateIntroVideo,
 } = require("../controllers/course/courseDetails.controller");
 
-router.get("/subtitle/video/:id", isEnrolled, getVideo);
-router.get("/subtitle/:id", isEnrolled, getSubtitle);
+router.get("/subtitle/video/:id", getVideo);
+router.get("/subtitle/:id", getSubtitle);
 router.get("/:id", getCourseDetails);
 router.post("/addRating", canRateCourse, addRating);
 router.post("/deleteRating", canDeleteRating, deleteRating);
-router.patch("/:id", updateIntroVideo)
+router.patch("/:id", updateIntroVideo);
 module.exports = router;
