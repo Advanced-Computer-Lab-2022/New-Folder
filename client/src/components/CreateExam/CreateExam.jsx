24d32286c7a2c9903e1d6ac17b9b73db3ff49a2d
Elsokkary101 <alyhassan123456@gmail.com>
2022-11-29 23:09:29 +0200

    adding create Exam css file and logic with pre processing to data provided to be ready to be sent to database

--- client/src/components/CreateExam/CreateExam.jsx
+++ client/src/components/CreateExam/CreateExam.jsx
@@ -1,7 +1,60 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import Col from 'react-bootstrap/esm/Col'
+import Row from 'react-bootstrap/esm/Row'
+import './CreateExam.css'
+import ExamForm from './ExamForm/ExamForm'
+
+
 const CreateExam = (props) => {
-  const subtitleID = props.subtitleID;
+  const [questionComponentArr , setQuestionComponentArr] = useState([{statement: "none", choices:['none','none','none','none'], correctIdx:0}]);
+  const [questionRecord , setQuestionRecord] = useState([null]);
 
-  return <div></div>;
-};
+  // const handleDataChange = (data,questionIndex) => {
+  //   let arr = [...questionComponentArr];
+  //   arr [questionIndex] = data;
+  //   setQuestionComponentArr(arr);
+  //   console.log(arr);
+  // }
 
-export default CreateExam;
+  const addAnotherQuestion = () => {
+    setQuestionRecord((oldstate)=> {
+      const temp = [...oldstate , null];
+      return temp;
+    });
+
+    let arr = questionComponentArr;
+    arr.push({statement: "none", choices:['none','none','none','none'], correctIdx:0});
+    setQuestionComponentArr(arr);
+    
+  }
+
+  // for testing questionComponentArr
+  useEffect (()=>{
+    console.log(questionComponentArr);
+  },[questionComponentArr])
+
+  return (
+    <div className='create-exam'>
+      <Row className="create-exam-header">
+        <Col><h4>Create New Quiz</h4></Col>
+        <Col><button type="button" class="btn btn-primary">Add Quiz</button></Col>
+      </Row>
+      <Row>
+        <Row className='form-area' lg={1}>
+          {questionRecord.map((questionComponent, index)=>{
+            return (<ExamForm key={index} questionIdx={index} questionComponentArr={questionComponentArr} setQuestionComponentArr={setQuestionComponentArr}/>);
+          })}
+        </Row>
+      </Row>
+
+      <Row className='btn-addmore'>
+        <Col><button type="button" class="btn btn-primary rounded-pill" onClick={addAnotherQuestion}><i class="bi bi-plus"></i>Add Another Question</button></Col>
+      </Row>
+
+
+    </div>
+  )
+}
+
+export default CreateExam
---
+++ client/src/components/CreateExam/ExamForm/ExamForm.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './ExamForm.css'
+import Form from 'react-bootstrap/Form'
+
+const ExamForm = (props) => {
+  const questionIdx = props.questionIdx;
+  const questionComponentArr = props.questionComponentArr;
+  const setQuestionComponentArr = props.setQuestionComponentArr;
+
+  const [firstChoice , setFirstChoice] = useState("[ None ]");
+  const [secondChoice , setSecondChoice] = useState("[ None ]");
+  const [thirdChoice , setThirdChoice] = useState("[ None ]");
+  const [forthChoice , setForthChoice] = useState("[ None ]");
+  const [correctAns , setCorrectAns] = useState(0);
+
+  const updateCorrectAnsData = (ans) => {
+    const temp = [...questionComponentArr];
+    temp[questionIdx].correctIdx = ans;
+    setQuestionComponentArr(temp);
+  }
+
+  const updateChoiceArray = (answer,index) =>{
+    const temp = [...questionComponentArr];
+    temp[questionIdx].choices[index] = answer;
+    setQuestionComponentArr(temp);
+  }
+
+  const updateStatement = (statement)=> {
+    const temp = [...questionComponentArr];
+    temp[questionIdx].statement = statement;
+    setQuestionComponentArr(temp);
+  }
+
+  return (
+    <div className="ExamForm">
+      <h4 className='question-number'>Question {questionIdx + 1}</h4>
+      <Form.Group>
+        <Form.Label>Statement</Form.Label>
+          <Form.Control size="sm" type="text" placeholder="Statement" onChange={(e)=> updateStatement(e.target.value)}/>
+        <div>
+        <Form.Label>Choices</Form.Label>
+          <Form.Control size="sm" type="text" placeholder="First Choice" onChange={(e)=> {updateChoiceArray(e.target.value,0); setFirstChoice(e.target.value)}} />
+          <Form.Control size="sm" type="text" placeholder="Second Choice" onChange={(e)=> {updateChoiceArray(e.target.value,1); setSecondChoice(e.target.value)}}/>
+          <Form.Control size="sm" type="text" placeholder="Third Choice" onChange={(e)=> {updateChoiceArray(e.target.value,2); setThirdChoice(e.target.value)}}/>
+          <Form.Control size="sm" type="text" placeholder="Forth Choice" onChange={(e)=> {updateChoiceArray(e.target.value,3); setForthChoice(e.target.value)}} />
+        <Form.Label>Correct Answer</Form.Label>
+          <div>
+            <Form.Check checked={correctAns == 0} type="radio" label={firstChoice} onClick={()=>{updateCorrectAnsData(0); setCorrectAns(0)}}/>
+            <Form.Check checked={correctAns == 1}  type="radio" label={secondChoice} onClick={()=>{updateCorrectAnsData(1); setCorrectAns(1)}} />
+            <Form.Check checked={correctAns == 2}  type="radio" label={thirdChoice} onClick={()=>{updateCorrectAnsData(2); setCorrectAns(2)}}/>
+            <Form.Check checked={correctAns == 3}  type="radio" label={forthChoice} onClick={()=>{updateCorrectAnsData(3); setCorrectAns(3)}} />   
+          </div>
+          
+        </div>
+      </Form.Group>
+    </div>
+  )
+}
+
+export default ExamForm
