0b24c80e0a1e23da050ad5149d5a3661c73e9d24
Ziad Othman <ziadsadek999@gmail.com>
2022-11-30 06:54:33 +0200

    Course details ui

--- client/src/components/CourseSummary/CourseSummary.jsx
+++ client/src/components/CourseSummary/CourseSummary.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
@@ -7,10 +7,73 @@ import RatingCard from "../../components/RatingCard/RatingCard";
 import Form from "react-bootstrap/Form";
 import ViewerContexts from "../../constants/ViewerContexts.json";
 import Button from "react-bootstrap/Button";
-
+import "./CourseSummary.css";
+import ReactStars from "react-rating-stars-component";
 function CourseSummary(props) {
+  let Stars = useMemo(() => {
+    return () => (
+      <ReactStars
+        count={5}
+        size={40}
+        isHalf={true}
+        activeColor="#ffd700"
+        value={props.course.totalRating}
+        edit={false}
+      />
+    );
+  }, [props.course.totalRating]);
   return (
     <>
+      <div id="courseSummaryContainer">
+        <div id="courseSummaryFirstRow">
+          <Row className="mb-0">
+            <Col md="auto">
+              <Image width={250} src={props.course.image} thumbnail />
+            </Col>
+            <Col>
+              <Stack gap={1} id="courseHeader">
+                <h2 id="courseTitle">{props.course.name}</h2>
+                <h5 id="courseInstructorName">Instructor</h5>
+                <div id="courseRatingStars">
+                  <Stars />
+                </div>
+              </Stack>
+            </Col>
+            <Col>
+              <Stack id="priceEnroll">
+                {props.vc !== ViewerContexts.nonEnrolledCorporateTrainee &&
+                props.vc !== ViewerContexts.enrolledTrainee ? (
+                  <h4 id="coursePrice">Price: {props.price ?? 0}</h4>
+                ) : null}
+                {props.vc === ViewerContexts.guest ? (
+                  <Button id="enrollButton" variant="dark">
+                    Enroll
+                  </Button>
+                ) : null}
+              </Stack>
+            </Col>
+          </Row>
+        </div>
+        <div>
+          <Row id="courseSummaryFirstRow">
+            <Col>
+              <Stack>
+                <div id="courseRating">
+                  <Stack direction="horizontal" gap={3}>
+                    <h3>{props.course.reviews?.length ?? 0} reviews</h3>
+                  </Stack>
+                </div>
+                <div id="courseRating">
+                  <Stack direction="horizontal" gap={3}>
+                    <h3>{props.course.subtitles?.length ?? 0} subtitles</h3>
+                  </Stack>
+                </div>
+              </Stack>
+            </Col>
+            <Col></Col>
+          </Row>
+        </div>
+      </div>
       <Row id="courseCardContainer">
         <Col md={2}>
           <Image width={250} src={props.course.image} thumbnail />
@@ -24,30 +87,9 @@ function CourseSummary(props) {
               <Stack gap={4} id="courseInfo">
                 <div id="courseRating">
                   <Stack direction="horizontal" gap={3}>
-                    <h3>{props.course.reviews?.length ?? 0} reviews</h3>
-                  </Stack>
-                </div>
-                <div id="courseRating">
-                  <Stack direction="horizontal" gap={3}>
-                    <h3>{props.course.subtitles?.length ?? 0} subtitles</h3>
-                  </Stack>
-                </div>
-                {props.vc !== ViewerContexts.nonEnrolledCorporateTrainee &&
-                props.vc !== ViewerContexts.enrolledTrainee ? (
-                  <div id="courseRating">
-                    <Stack direction="horizontal" gap={3}>
-                      <h3>Price: {props.price ?? 0}</h3>
-                    </Stack>
-                  </div>
-                ) : null}
-                <div id="courseRating">
-                  <Stack direction="horizontal" gap={3}>
                     <RatingCard courseId={props.courseId} vc={props.vc} />
                   </Stack>
                 </div>
-                {props.vc === ViewerContexts.guest ? (
-                  <Button>Enroll</Button>
-                ) : null}
               </Stack>
             </Col>
             <Col>
