ebc284680b5a406623c48bbf5614f224bf2ed59f
Ziad Othman <ziadsadek999@gmail.com>
2022-12-17 23:14:26 +0200

    Follow up front end

--- client/src/components/ProblemCard/ProblemCard.jsx
+++ client/src/components/ProblemCard/ProblemCard.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, Button, Card, Col, Row } from "react-bootstrap";
 import { MdOutlinePending } from "react-icons/md";
 import { AiOutlineEyeInvisible } from "react-icons/ai";
 import { FiCheckCircle } from "react-icons/fi";
@@ -9,9 +9,42 @@ import UserTypes from "../../constants/UserTypes.json";
 import ProblemTypes from "../../constants/ProblemTypes.json";
 import { HiOutlineWrenchScrewdriver } from "react-icons/hi2";
 import { GiTwoCoins } from "react-icons/gi";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 import "./ProblemCard.css";
+import { useEffect } from "react";
 function ProblemCard(props) {
   const { problem } = props;
+  const [followups, setFollowups] = useState([]);
+  const [editing, setEditing] = useState(false);
+  const [validated, setValidated] = useState(false);
+  const [newFollowup, setNewFollowup] = useState(null);
+  useEffect(() => {
+    setFollowups(problem.followups ?? []);
+  }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    } else {
+      setFollowups([...followups, newFollowup]);
+      setValidated(false);
+      setNewFollowup(null);
+      setEditing(false);
+    }
+  };
+  const cancel = () => {
+    setValidated(false);
+    setNewFollowup(null);
+    setEditing(false);
+  };
   return (
     <>
       <div id="problemContainer">
@@ -78,6 +111,71 @@ function ProblemCard(props) {
           </Col>
         </Row>
         <div id="problemCard">{problem.body}</div>
+        <Accordion id="reportAccordionWrapper">
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Followups ({followups.length ?? 0})</Typography>
+          </AccordionSummary>
+          <AccordionDetails id="followupsList">
+            <ol>
+              {followups.map((followup) => {
+                return <li>{followup}</li>;
+              })}
+            </ol>
+            {editing ? (
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Group
+                  className="mb-3"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label as={"h6"}>Add a followup</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    required
+                    onChange={(e) => setNewFollowup(e.target.value)}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Followup cannot be empty
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <div id="followupFormFooter">
+                  <Button
+                    variant="secondary"
+                    onClick={cancel}
+                    className="followupFormButton"
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    className="followupFormButton"
+                  >
+                    Add
+                  </Button>
+                </div>
+              </Form>
+            ) : null}
+            {problem.status !== ProblemStatus.resolved &&
+            !editing &&
+            ReactSession.get("userType") !== UserTypes.admin ? (
+              <Card
+                id="addFollowupButton"
+                onClick={() => {
+                  setEditing(true);
+                }}
+              >
+                <div id="addIconWrapper">
+                  <AiOutlinePlusCircle />
+                </div>
+                &nbsp;Add a followup
+              </Card>
+            ) : null}
+          </AccordionDetails>
+        </Accordion>
       </div>
     </>
   );
