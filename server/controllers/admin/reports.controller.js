64c2586cacf5ec52dbc4de66f574812a02398ec1
abd0123 <abdof1723@gmail.com>
2022-12-17 20:12:07 +0200

    resolved vs unresolved reports

--- server/controllers/admin/reports.controller.js
+++ server/controllers/admin/reports.controller.js
@@ -2,9 +2,11 @@ const Report = require("../../models/Report.model");
 const status = require("../../report-status.json");
 exports.getReports = async (req, res) => {
   try {
-    const reports = await Report.find({ status: { $ne: status.resolved } });
-    res.status(200).json(reports);
+    const unresolved = await Report.find({ status: { $ne: status.resolved } });
+    const resolved = await Report.find({ status: status.resolved });
+    console.log(unresolved);
+    res.status(200).json({ unresolved: unresolved, resolved: resolved });
   } catch (err) {
     res.status(400);
-  }
+  } 
 };
