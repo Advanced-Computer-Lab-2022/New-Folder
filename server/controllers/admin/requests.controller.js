59c9326f39cd49786c33bc050949f4ec1f6ec2d4
Ziad Othman <ziadsadek999@gmail.com>
2022-12-20 23:15:15 +0200

    await bug

--- server/controllers/admin/requests.controller.js
+++ server/controllers/admin/requests.controller.js
@@ -10,18 +10,18 @@ const getRequests = async (req, res) => {
   }
 };
 
-const updateAndDelete = async(course, request)=>{
-    let pendingTrainees = [];
-    console.log(request.userId);
-    for (let i = 0; i < course.pendingTrainees.length; i++) {
-      if (course.pendingTrainees[i].toString() !== request.userId.toString()) {
-        pendingTrainees.push(course.pendingTrainees[i]);
-      }
+const updateAndDelete = async (course, request) => {
+  let pendingTrainees = [];
+  console.log(request.userId);
+  for (let i = 0; i < course.pendingTrainees.length; i++) {
+    if (course.pendingTrainees[i].toString() !== request.userId.toString()) {
+      pendingTrainees.push(course.pendingTrainees[i]);
     }
-    course.pendingTrainees = pendingTrainees;
-    await course.save();
-    await Request.deleteOne({ id: request.id });
-}
+  }
+  course.pendingTrainees = pendingTrainees;
+  await course.save();
+  await Request.deleteOne({ id: request.id });
+};
 
 const deleteRequest = async (req, res) => {
   try {
@@ -29,28 +29,28 @@ const deleteRequest = async (req, res) => {
     const request = await Request.findById(req.params.id);
     console.log(request);
     const course = await Course.findById(request.courseId);
-    updateAndDelete(course, request);
+    await updateAndDelete(course, request);
     res.status(200).send("deleted successfully");
   } catch (err) {
     res.status(500);
   }
 };
 
-const approveRequest = async(req, res)=> {
-    try{
-        const request = await Request.findById(req.params.id);
-        const course = await Course.findById(request.courseId);
-        const trainees = [...course.trainees];
-        trainees.push(request.userId);
-        course.trainees = trainees;
-        updateAndDelete(course, request);
-        const trainee = await Trainee.findById(request.userId);
-        trainee.courses.push(request.courseId);
-        trainee.save();
-        res.status(200).send("Approved Successfully");
-    }catch(err){
-        res.status(500);
-    }
-}
+const approveRequest = async (req, res) => {
+  try {
+    const request = await Request.findById(req.params.id);
+    const course = await Course.findById(request.courseId);
+    const trainees = [...course.trainees];
+    trainees.push(request.userId);
+    course.trainees = trainees;
+    await updateAndDelete(course, request);
+    const trainee = await Trainee.findById(request.userId);
+    trainee.courses.push(request.courseId);
+    trainee.save();
+    res.status(200).send("Approved Successfully");
+  } catch (err) {
+    res.status(500);
+  }
+};
 
 module.exports = { getRequests, deleteRequest, approveRequest };
