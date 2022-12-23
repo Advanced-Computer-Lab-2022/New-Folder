ef4722c5b407dbdc3fadd098ab3eea81a18fb3fa
Ziad Othman <ziadsadek999@gmail.com>
2022-12-23 23:41:11 +0200

    Admin set promotion

--- client/src/components/AdminSetPromotion/AdminSetPromotion.jsx
+++ client/src/components/AdminSetPromotion/AdminSetPromotion.jsx
@@ -16,7 +16,8 @@ import Autocomplete from "@mui/material/Autocomplete";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import { FormControl, FormControlLabel, FormGroup } from "@mui/material";
-
+import Spinner from "react-bootstrap/Spinner";
+import SuccessFeedback from "../SuccessFeedback/SuccessFeedback";
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
@@ -30,6 +31,9 @@ function AdminSetPromotion(props) {
   const [courses, setCourses] = useState([]);
   const [selectedCourses, setSelectedCourses] = useState([]);
   const [allSelected, setAllSelected] = useState(false);
+  const [editing, setEditing] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
   const fetchData = async () => {
     try {
       const fetchedCourses = await fetchExploreData();
@@ -41,8 +45,7 @@ function AdminSetPromotion(props) {
   useEffect(() => {
     fetchData();
   }, []);
-  const save = async (e) => {
-    e.preventDefault();
+  const save = async () => {
     if (selectedCourses.length <= 0) {
       setSelectError("Please select at least 1 course");
       return;
@@ -67,23 +70,29 @@ function AdminSetPromotion(props) {
       setPercentageError("Percentage must be between 0-100!");
       return;
     }
+    setLoading(true);
     setPercentageError(null);
-    setShow(false);
     const addedPromotion = {
       startDate: new Date(startDate).getTime(),
       endDate: new Date(endDate).getTime(),
       percentage: newPercentage,
     };
+    const coursesIds = selectedCourses.map((course) => course._id);
+    postMultiPromotion(coursesIds, addedPromotion).then(() => {});
     setNewPercentage(null);
     setRange(null);
     setSelectedCourses([]);
     setAllSelected(false);
-    const coursesIds = selectedCourses.map((course) => course._id);
-    postMultiPromotion(coursesIds, addedPromotion).then(
-      window.location.reload()
-    );
+    setEditing(false);
+    setSuccess(true);
+    setLoading(false);
+    console.log("mango");
   };
-  const cancel = async () => {
+  const cancel = () => {
+    if (success) {
+      window.location.reload();
+      return;
+    }
     setPercentageError(null);
     setDateError(null);
     setShow(false);
@@ -92,120 +101,158 @@ function AdminSetPromotion(props) {
     setSelectedCourses([]);
     setAllSelected(false);
     setSelectError(null);
+    setEditing(true);
+    setSuccess(false);
+    setShow(false);
+    setLoading(false);
   };
   return (
     <div>
       <>
-        <Modal show={show} onHide={() => setShow(false)} size={"lg"} centered>
-          <Modal.Header>
-            <Modal.Title>Set promotion</Modal.Title>
-          </Modal.Header>
-          <div>
-            <Autocomplete
-              multiple
-              disabled={allSelected}
-              id="checkboxes-tags-demo"
-              options={courses}
-              disableCloseOnSelect
-              value={allSelected ? [] : selectedCourses}
-              onChange={(event, value) => {
-                setSelectedCourses(value);
-                console.log(value);
-              }}
-              onre
-              getOptionLabel={(option) => option.name}
-              renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                  <Checkbox
-                    icon={icon}
-                    checkedIcon={checkedIcon}
-                    style={{ marginRight: 8 }}
-                    checked={selected}
-                  />
-                  {option.name}
-                </li>
-              )}
-              style={{ width: 400 }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  id="standard-basic"
-                  variant="standard"
-                  label={
-                    allSelected ? "All courses selected" : "Select courses"
-                  }
+        <Modal
+          show={show}
+          onHide={cancel}
+          size={"lg"}
+          centered
+          backdrop={loading ? "static" : "dynamic"}
+        >
+          {!editing ? (
+            <>
+              {success ? (
+                <SuccessFeedback
+                  msg="Promotion added successfully!"
+                  onClose={cancel}
                 />
-              )}
-            />
-
-            <FormControl component="fieldset">
-              <FormGroup aria-label="position" row sx={{ margin: 0 }}>
-                <FormControlLabel
-                  value="end"
-                  control={
-                    <Checkbox
-                      icon={icon}
-                      checkedIcon={checkedIcon}
-                      style={{ marginRight: 8 }}
-                      checked={allSelected}
-                      onChange={() => {
-                        allSelected
-                          ? setSelectedCourses([])
-                          : setSelectedCourses(courses);
-                        setAllSelected(!allSelected);
-                      }}
-                      sx={{ padding: 0, marginLeft: 1 }}
-                    />
-                  }
-                  label="Select all"
-                  labelPlacement="end"
-                />
-              </FormGroup>
-            </FormControl>
-            {selectError ? (
-              <p className="promotionError">{selectError}</p>
-            ) : null}
-          </div>
-          <Modal.Body>
-            <h6 id="addPromotionHeader">Select start and end dates:</h6>
-            <Stack direction="vertical" gap={1}>
-              <DayPicker
-                mode="range"
-                selected={range}
-                onSelect={setRange}
-                disabled={[
-                  {
-                    from: new Date(1900, 4, 18),
-                    to: new Date(
-                      new Date().getFullYear(),
-                      new Date().getMonth(),
-                      new Date().getDate() - 1
-                    ),
-                  },
-                ]}
-              />
-            </Stack>
-            {dateError ? <p className="promotionError">{dateError}</p> : null}
-            <Form.Group as={Col}>
-              <Form.Control
-                type="number"
-                placeholder="Promotion %"
-                value={newPercentage}
-                onChange={(e) => setNewPercentage(e.target.value)}
-              />
-              {percentageError ? (
-                <p className="promotionError">{percentageError}</p>
               ) : null}
-            </Form.Group>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={cancel}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={save}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
+            </>
+          ) : (
+            <>
+              <Modal.Header>
+                <Modal.Title>Set promotion</Modal.Title>
+              </Modal.Header>
+              <div>
+                <Autocomplete
+                  multiple
+                  disabled={allSelected}
+                  id="checkboxes-tags-demo"
+                  options={courses}
+                  disableCloseOnSelect
+                  value={allSelected ? [] : selectedCourses}
+                  onChange={(event, value) => {
+                    setSelectedCourses(value);
+                    console.log(value);
+                  }}
+                  onre
+                  getOptionLabel={(option) => option.name}
+                  renderOption={(props, option, { selected }) => (
+                    <li {...props}>
+                      <Checkbox
+                        icon={icon}
+                        checkedIcon={checkedIcon}
+                        style={{ marginRight: 8 }}
+                        checked={selected}
+                      />
+                      {option.name}
+                    </li>
+                  )}
+                  style={{ width: 400 }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      id="standard-basic"
+                      variant="standard"
+                      label={
+                        allSelected ? "All courses selected" : "Select courses"
+                      }
+                    />
+                  )}
+                />
+
+                <FormControl component="fieldset">
+                  <FormGroup aria-label="position" row sx={{ margin: 0 }}>
+                    <FormControlLabel
+                      value="end"
+                      control={
+                        <Checkbox
+                          icon={icon}
+                          checkedIcon={checkedIcon}
+                          style={{ marginRight: 8 }}
+                          checked={allSelected}
+                          onChange={() => {
+                            allSelected
+                              ? setSelectedCourses([])
+                              : setSelectedCourses(courses);
+                            setAllSelected(!allSelected);
+                          }}
+                          sx={{ padding: 0, marginLeft: 1 }}
+                        />
+                      }
+                      label="Select all"
+                      labelPlacement="end"
+                    />
+                  </FormGroup>
+                </FormControl>
+                {selectError ? (
+                  <p className="promotionError">{selectError}</p>
+                ) : null}
+              </div>
+              <Modal.Body>
+                <h6 id="addPromotionHeader">Select start and end dates:</h6>
+                <Stack direction="vertical" gap={1}>
+                  <DayPicker
+                    mode="range"
+                    selected={range}
+                    onSelect={setRange}
+                    disabled={[
+                      {
+                        from: new Date(1900, 4, 18),
+                        to: new Date(
+                          new Date().getFullYear(),
+                          new Date().getMonth(),
+                          new Date().getDate() - 1
+                        ),
+                      },
+                    ]}
+                  />
+                </Stack>
+                {dateError ? (
+                  <p className="promotionError">{dateError}</p>
+                ) : null}
+                <Form.Group as={Col}>
+                  <Form.Control
+                    type="number"
+                    placeholder="Promotion %"
+                    value={newPercentage}
+                    onChange={(e) => setNewPercentage(e.target.value)}
+                  />
+                  {percentageError ? (
+                    <p className="promotionError">{percentageError}</p>
+                  ) : null}
+                </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={cancel} disabled={loading}>
+                  Close
+                </Button>
+                {loading ? (
+                  <Button variant="primary" disabled>
+                    <Spinner
+                      as="span"
+                      animation="border"
+                      size="sm"
+                      role="status"
+                      aria-hidden="true"
+                    />
+                    {" Saving..."}
+                  </Button>
+                ) : (
+                  <Button variant="primary" onClick={save}>
+                    Save Changes
+                  </Button>
+                )}
+              </Modal.Footer>
+            </>
+          )}
         </Modal>
       </>
     </div>
