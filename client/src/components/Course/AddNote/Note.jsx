8fed01a404ac723f6c4babfbd3643879fbff3059
Elsokkary101 <alyhassan123456@gmail.com>
2022-12-23 16:16:59 +0200

    download PDF bug has been resolved

--- client/src/components/Course/AddNote/Note.jsx
+++ client/src/components/Course/AddNote/Note.jsx
@@ -6,6 +6,8 @@ import { FetchNote, postNote } from "../../../network";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import "./Note.css";
 import PDFnote from "../../PDF/PDFnote";
+import jsPDF from 'jspdf';
+
 const Note = (props) => {
   const conID = props.conID;
   const conTitle = props.conTitle;
@@ -14,6 +16,16 @@ const Note = (props) => {
 
 //   not used but could be used anytime later
   const [download, setDownload] = useState(false);
+
+  const generatePDF = (noteTitle, noteContent) => {
+    var doc = new jsPDF();
+    doc.setFontSize(20);
+    doc.text(noteTitle, 30,25);
+    doc.setFontSize(15);
+    const s = doc.splitTextToSize(noteContent,200);
+    doc.text(s, 15, 50);
+    doc.save(noteTitle + "notes");
+  }
 
   const getNote = async () => {
     const traineeNote = await FetchNote(conID);
@@ -45,7 +57,7 @@ const Note = (props) => {
         <Button variant="success" onClick={(e) => handleSubmitNote()}>
           Save
         </Button>
-        <PDFDownloadLink
+        {/* <PDFDownloadLink
           document={<PDFnote conTitle={conTitle} notes={noteValue} />}
           fileName={conTitle + " notes"}
         >
@@ -59,8 +71,12 @@ const Note = (props) => {
                 Download as <strong>PDF</strong>
               </Button>
             )
-          }
-        </PDFDownloadLink>
+          } */}
+        {/* </PDFDownloadLink> */}
+        <Button variant="success" onClick={(e) => generatePDF(conTitle , noteValue)}>
+                Download as <strong>PDF</strong>
+        </Button>
+
       </div>
     </Col>
   );
