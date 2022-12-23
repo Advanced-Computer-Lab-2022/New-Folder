95299ce7533458336851bf9e9d2de18606ded1a9
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-12-23 20:57:43 +0200

    graph

--- client/src/components/EarningsGraph/EarningsGraph.jsx
+++ client/src/components/EarningsGraph/EarningsGraph.jsx
@@ -1,5 +1,58 @@
+import React from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Chart } from "react-chartjs-2";
+import { Line, Bar } from "react-chartjs-2";
+
 const EarningsGraph = () => {
-  return <div></div>;
+  const state = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Rainfall",
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  };
+  return (
+    <div>
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: [
+            "Jan",
+            "Feb",
+            "Mar",
+            "Apr",
+            "May",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ],
+          datasets: [
+            {
+              id: 1,
+              backgroundColor: "rgba(0,0,0,1)",
+              borderColor: "rgba(0,0,0,1)",
+              label: "usd",
+              data: [5, 3, 7],
+            },
+            {
+              id: 2,
+              label: "egp",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
+    </div>
+  );
 };
 
 export default EarningsGraph;
--- client/src/pages/user/instructor/Earnings.jsx
+++ client/src/pages/user/instructor/Earnings.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { getEarnings } from "../../../network";
 import EarningsCard from "../../../components/EarningsCard/EarningsCard";
 import EarningsGraph from "../../../components/EarningsGraph/EarningsGraph";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Earnings = () => {
   const [earnings, setEarnings] = useState([]);
@@ -13,12 +16,17 @@ const Earnings = () => {
     fetchData();
   }, []);
   return (
-    <div>
+    <Container>
+      <div></div>
+      <Row>
+        <Col>Earnings</Col>
+        <Col>Earnings</Col>
+      </Row>
       <EarningsGraph earnings={earnings} />
       {earnings.map((earning) => (
         <EarningsCard year={earning.year} months={earning.months} />
       ))}
-    </div>
+    </Container>
   );
 };
 
