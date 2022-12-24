5ee0504ed285a913d666f83bcc16420df83c4541
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-12-24 02:37:48 +0200

    Update Earnings.jsx

--- client/src/pages/user/instructor/Earnings.jsx
+++ client/src/pages/user/instructor/Earnings.jsx
@@ -4,15 +4,17 @@ import EarningsCard from "../../../components/EarningsCard/EarningsCard";
 import EarningsGraph from "../../../components/EarningsGraph/EarningsGraph";
 import PageHeader from "../../../components/PageHeader/PageHeader";
 import EarningsDropdown from "../../../components/EarningsDropdown/EarningsDropdown";
-import { Image, Stack } from "react-bootstrap";
+import { Image, Spinner, Stack } from "react-bootstrap";
 
 const Earnings = () => {
   const [earnings, setEarnings] = useState([]);
   const [year, setYear] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [graphData, setGraphData] = useState(null);
   const fetchData = async () => {
     const data = await getEarnings();
     setEarnings(data);
+    setLoading(false);
     if (data.length > 0) setYear(data[0]);
   };
   useEffect(() => {
@@ -34,21 +36,29 @@ const Earnings = () => {
   }, [year]);
   return (
     <div>
-      <PageHeader
-        pageName="Earnings"
-        extra={<EarningsDropdown years={earnings} setYear={setYear} />}
-      />
-      {earnings.length > 0 ? (
-        <div className="mt-5">
-          <EarningsGraph data={graphData} />
-          <EarningsCard months={year.months} />
-        </div>
+      {loading ? (
+        <Stack className="m-4">
+          <Spinner animation="border" />
+        </Stack>
       ) : (
-        <div className="pt-3">
-          <Stack className="mt-5" gap={3}>
-            <Image width={"30%"} src="/assets/Banknote.gif" />
-            <h2 className="m-auto">You don't have any earnings yet</h2>
-          </Stack>
+        <div>
+          <PageHeader
+            pageName="Earnings"
+            extra={<EarningsDropdown years={earnings} setYear={setYear} />}
+          />
+          {earnings.length > 0 ? (
+            <div className="mt-5">
+              <EarningsGraph data={graphData} />
+              <EarningsCard months={year.months} />
+            </div>
+          ) : (
+            <div className="pt-3">
+              <Stack className="mt-5" gap={3}>
+                <Image width={"30%"} src="/assets/Banknote.gif" />
+                <h2 className="m-auto">You don't have any earnings yet</h2>
+              </Stack>
+            </div>
+          )}
         </div>
       )}
     </div>
