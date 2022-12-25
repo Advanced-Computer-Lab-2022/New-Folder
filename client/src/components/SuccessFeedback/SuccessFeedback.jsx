6cf3f97f382c3d6167256bc59a51dfd0101b11c8
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2022-12-25 04:23:04 +0200

    bug fix

--- client/src/components/SuccessFeedback/SuccessFeedback.jsx
+++ client/src/components/SuccessFeedback/SuccessFeedback.jsx
@@ -4,8 +4,7 @@ import Button from "react-bootstrap/esm/Button";
 import { Checkmark } from "react-checkmark";
 import "./SuccessFeedback.css";
 function SuccessFeedback(props) {
-  const { msg, onClose } = props;
-  setTimeout(onClose, 3000);
+  const { msg } = props;
   return (
     <div id="successFeedbackContainer">
       <h3 className="blackTxt" id="successHeader">
