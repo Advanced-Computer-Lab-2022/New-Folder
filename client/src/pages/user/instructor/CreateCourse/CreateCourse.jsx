12f12fc7f146fcfbabdcf6f599d10f5080c67ac7
Ziad Othman <69426929+Itchyyy110@users.noreply.github.com>
2022-12-25 20:57:09 +0200

    Styling done

--- client/src/pages/user/instructor/CreateCourse/CreateCourse.jsx
+++ client/src/pages/user/instructor/CreateCourse/CreateCourse.jsx
@@ -10,7 +10,7 @@ import Row from "react-bootstrap/Row";
 import CloseButton from "react-bootstrap/CloseButton";
 import ContractCard from "../../../../components/ContractCard/ContractCard";
 import PageHeader from "../../../../components/PageHeader/PageHeader";
-
+import "./CreateCourse.css";
 const countries = CountryCurrency.getAllISOCodes();
 
 function CreateCourse() {
@@ -24,7 +24,6 @@ function CreateCourse() {
   const [introVideo, setIntroVideo] = useState("");
   const [courseImage, setCourseImage] = useState("");
   const [validated, setValidated] = useState(false);
-
   //terms and conditions
   const [show, setShow] = useState(false);
 
@@ -65,153 +64,159 @@ function CreateCourse() {
   };
 
   return (
-    <div>
+    <>
       <PageHeader pageName="Create course" />
       <Form onSubmit={onSubmit} noValidate validated={validated}>
-        <Container className="mt-4">
-          <Col lg="5">
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                value={name}
-                placeholder="Enter Course Name"
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                value={field}
-                placeholder="Enter Course Field"
-                onChange={(e) => setField(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                value={introVideo}
-                placeholder="Add Course preview link"
-                onChange={(e) => setIntroVideo(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                value={courseImage}
-                placeholder="Add Course Image Link"
-                onChange={(e) => setCourseImage(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                as="textarea"
-                rows={3}
-                value={description}
-                placeholder="Enter Course Description"
-                onChange={(e) => setDescription(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            {subtitles.map((sub, index) => {
-              return (
-                <Row>
-                  <Form.Group className="mb-3" as={Col}>
-                    <Form.Control
-                      type="text"
-                      id={"subtitle" + index}
-                      value={subtitles[index]}
-                      name={"subtitle" + index}
-                      placeholder="Enter subtitle"
-                      onChange={(e) => {
-                        const temp = [...subtitles];
-                        temp[index] = e.target.value;
-                        setSubtitles(temp);
-                      }}
-                      required
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      This field is required
-                    </Form.Control.Feedback>
-                  </Form.Group>
+        <Col md="4" id="createCourseWrapper">
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              value={name}
+              placeholder="Course Name"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              value={field}
+              placeholder="Course Field"
+              onChange={(e) => setField(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              value={introVideo}
+              placeholder="Course preview link"
+              onChange={(e) => setIntroVideo(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              value={courseImage}
+              placeholder="Course Image Link"
+              onChange={(e) => setCourseImage(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={description}
+              placeholder="Course Description"
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          {subtitles.map((sub, index) => {
+            return (
+              <Row>
+                <Form.Group className="mb-3" as={Col} md="11">
+                  <Form.Control
+                    type="text"
+                    id={"subtitle" + index}
+                    value={subtitles[index]}
+                    name={"subtitle" + index}
+                    placeholder="Enter subtitle"
+                    onChange={(e) => {
+                      const temp = [...subtitles];
+                      temp[index] = e.target.value;
+                      setSubtitles(temp);
+                    }}
+                    required
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    This field is required
+                  </Form.Control.Feedback>
+                </Form.Group>
 
-                  <Form.Group as={Col} className="mt-2">
-                    <CloseButton onClick={() => removeSubtitle(index)} />
-                  </Form.Group>
-                </Row>
-              );
-            })}
-            <Form.Group className="mb-3">
-              <Button
-                variant="dark"
-                onClick={(e) => {
-                  setSubtitles([...subtitles, ""]);
-                }}
-              >
-                Add Subtitle
-              </Button>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Control
-                type="text"
-                id="magnitude"
-                name="magnitude"
-                value={magnitude}
-                placeholder="Enter Course price"
-                onChange={(e) => setMagnitude(e.target.value)}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                This field is required
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Select
-                id="currency"
-                name="currency"
-                onChange={(e) => {
-                  console.log(e.target.value);
-                  setCurrency(e.target.value);
-                }}
-              >
-                {countries.map((country) => (
-                  <option
-                    selected={country.currency === currency}
-                    value={country.currency}
-                  >
-                    {country.countryName + " (" + country.currency + ")"}
-                  </option>
-                ))}
-              </Form.Select>
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Button variant="dark" type="submit">
-                submit
-              </Button>
-              <ContractCard submit={submit} show={show} setShow={setShow} />
-            </Form.Group>
-          </Col>
-        </Container>
+                <Form.Group as={Col} className="mt-2">
+                  <CloseButton onClick={() => removeSubtitle(index)} />
+                </Form.Group>
+              </Row>
+            );
+          })}
+          <Form.Group className="mb-3">
+            <Button
+              id="addSubtitleButton"
+              onClick={(e) => {
+                setSubtitles([...subtitles, ""]);
+              }}
+            >
+              Add Subtitle
+            </Button>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Control
+              type="text"
+              id="magnitude"
+              name="magnitude"
+              value={magnitude}
+              placeholder="Course price"
+              onChange={(e) => setMagnitude(e.target.value)}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              This field is required
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Select
+              id="currency"
+              name="currency"
+              onChange={(e) => {
+                console.log(e.target.value);
+                setCurrency(e.target.value);
+              }}
+            >
+              {countries.map((country) => (
+                <option
+                  selected={country.currency === currency}
+                  value={country.currency}
+                >
+                  {country.countryName + " (" + country.currency + ")"}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Agree to terms and conditions"
+              feedback="You must agree before submitting."
+              feedbackType="invalid"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Button type="submit" id="createCourseSaveButton">
+              Create course
+            </Button>
+            <ContractCard submit={submit} show={show} setShow={setShow} />
+          </Form.Group>
+        </Col>
       </Form>
-    </div>
+    </>
   );
 }
 
