fbc165e38f45a7d3bd373924fcd2d150f88b2902
Ahmed Monier <69426929+Itchyyy110@users.noreply.github.com>
2022-12-26 19:57:17 +0200

    Create courseUtils.controller.js

---
+++ server/controllers/course/courseUtils.controller.js
@@ -0,0 +1,51 @@
+const Course = require("../../models/Course.model");
+const Subtitle = require("../../models/Subtitle.model");
+const Content = require("../../models/Content.model");
+const constants = require("../constants.json");
+
+const coursePrice = async (course) => {
+  const course = await Course.findById(course._id);
+  let discount = course.promotion;
+  let priceAfterPromotion = parseFloat(course.price.magnitude);
+  let hasPromotion = false;
+  if (discount) {
+    let now = Date.now();
+    if (discount.startDate <= now && discount.endDate > now) {
+      priceAfterPromotion *= 1 - discount.percentage / 100;
+      hasPromotion = true;
+    }
+  }
+  return {
+    hasPromotion,
+    priceBeforePromotion: course.price.magnitude,
+    priceAfterPromotion,
+    currency: course.price.currency,
+  };
+};
+
+const courseDuration = async (course) => {
+  const course = await Course.findById(course._id);
+  let allContent = [];
+  course?.subtitles?.array.forEach(async (sub) => {
+    let subtitle = await Subtitle.findById(sub);
+    allContent = allContent.concat(
+      subtitle?.subTitle_Content?.filter(
+        (content) => content.type === constants.content
+      )
+    );
+  });
+  let totalDurationInSeconds =
+    allContent?.reduce(async (total, c) => {
+      let content = await Content.findById(c.subTitle_Content_id);
+      return total + content.duration;
+    }, 0) ?? 0;
+
+  const hrs = Math.floor(totalDurationInSeconds / 3600);
+  totalDurationInSeconds -= hrs * 3600;
+  const mins = Math.floor(totalDurationInSeconds / 60);
+  totalDurationInSeconds -= mins * 60;
+  const secs = totalDurationInSeconds;
+  return { hrs, mins, secs };
+};
+
+module.exports = { coursePrice, courseDuration };
