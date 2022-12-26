04831c7f90e9bd562a1e81096f74800258e4eff4
Ahmed Monier <69426929+Itchyyy110@users.noreply.github.com>
2022-12-26 20:09:14 +0200

    Update explore.controller.js

--- server/controllers/guest/explore.controller.js
+++ server/controllers/guest/explore.controller.js
@@ -1,9 +1,31 @@
 const Course = require("../../models/Course.model");
+const {
+  coursePrice,
+  courseDuration,
+} = require("../course/courseUtils.controller");
 
 exports.getExplore = async (req, res) => {
   const courses = await Course.find();
   courses.sort(
     (course1, course2) => course2.trainees.length - course1.trainees.length
   );
-  res.send(courses);
+  let coursePrices = await Promise.all(
+    courses.map((course) => coursePrice(course))
+  );
+  let courseDurations = await Promise.all(
+    courses.map((course) => courseDuration(course))
+  );
+  let coursesFormatted = courses.map((course, index) => {
+    const { name, subject, totalRating, image, instructorInfo } = course;
+    return {
+      price: coursePrices[index],
+      duration: courseDurations[index],
+      instructorName: instructorInfo.instructorName,
+      name,
+      subject,
+      totalRating,
+      image,
+    };
+  });
+  res.send(coursesFormatted);
 };
