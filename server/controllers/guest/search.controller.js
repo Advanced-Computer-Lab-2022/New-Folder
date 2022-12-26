1c0c59194e018a39e18f984c42fbde7ff3418bba
Ahmed Monier <69426929+Itchyyy110@users.noreply.github.com>
2022-12-26 20:12:43 +0200

    Update search.controller.js

--- server/controllers/guest/search.controller.js
+++ server/controllers/guest/search.controller.js
@@ -1,4 +1,8 @@
 const Course = require("../../models/Course.model");
+const {
+  coursePrice,
+  courseDuration,
+} = require("../course/courseUtils.controller");
 
 exports.postSearch = async (req, res) => {
   const courses = await Course.aggregate([
@@ -12,5 +16,23 @@ exports.postSearch = async (req, res) => {
       },
     },
   ]);
-  res.send(courses);
+  let coursePrices = await Promise.all(
+    courses.map((course) => coursePrice(course))
+  );
+  let courseDurations = await Promise.all(
+    courses.map((course) => courseDuration(course))
+  );
+  let coursesFormatted = courses.map((course, index) => {
+    const { name, subject, totalRating, image, instructorInfo } = course;
+    return {
+      price: coursePrices[index],
+      duration: courseDurations[index],
+      instructorName: instructorInfo.instructorName,
+      name,
+      subject,
+      totalRating,
+      image,
+    };
+  });
+  res.send(coursesFormatted);
 };
