7a0a3d19c926341ecfcd9bb4dd935b1d2e117f89
Ziad Othman <68449722+ziadsadek999@users.noreply.github.com>
2022-12-27 05:33:02 +0200

    Search bar styling

--- client/src/pages/user/instructor/MyCourses/MyCourses.jsx
+++ client/src/pages/user/instructor/MyCourses/MyCourses.jsx
@@ -14,7 +14,9 @@ import Row from "react-bootstrap/Row";
 import "./MyCourses.css";
 import { Autocomplete, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-
+import PageHeader from "../../../../components/PageHeader/PageHeader";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import colors from "../../../../colors.json";
 const MyCourses = () => {
   const [courses, setCourses] = useState([]);
   const [filteredCourses, setFilteredCourses] = useState(courses);
@@ -58,83 +60,109 @@ const MyCourses = () => {
     fetchData();
     clearFilters();
   }, []);
-
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      secondary: {
+        main: "#fff",
+      },
+    },
+  });
   return (
-    <Row>
-      <Autocomplete
-        freeSolo
-        id="free-solo-2-demo"
-        disableClearable
-        options={courses}
-        getOptionLabel={(option) => option.name}
-        onChange={(event, value) => {
-          navigate("/course/" + value.id);
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search input"
-            InputProps={{
-              ...params.InputProps,
-              type: "search",
+    <>
+      <PageHeader
+        pageName="My courses"
+        extra={
+          <Autocomplete
+            freeSolo
+            id="free-solo-2-demo"
+            disableClearable
+            options={courses}
+            getOptionLabel={(option) => option.name}
+            onChange={(event, value) => {
+              navigate("/course/" + value.id);
             }}
+            renderInput={(params) => (
+              <ThemeProvider theme={theme}>
+                <TextField
+                  {...params}
+                  variant="filled"
+                  size="small"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                  label="Search my courses"
+                  sx={{
+                    input: { color: "white" },
+                    label: { color: "white" },
+                    borderColor: "white",
+                    marginTop: 0.5,
+                  }}
+                />
+              </ThemeProvider>
+            )}
           />
-        )}
+        }
       />
-      <Row className="m-4">
-        <Form.Group as={Col}>
-          <Form.Control
-            type="number"
-            placeholder="min price"
-            value={minPrice}
-            onChange={(e) => setMinPrice(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Control
-            type="number"
-            placeholder="max price"
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Control
-            type="number"
-            placeholder="rating"
-            value={rating}
-            onChange={(e) => setRating(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Form.Control
-            type="text"
-            placeholder="subject"
-            value={subject}
-            onChange={(e) => setSubject(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group as={Col}>
-          <Button variant="dark" className="me-4" onClick={filter}>
-            Filter
-          </Button>
-          <Button variant="dark" onClick={clearFilters}>
-            Clear filters
-          </Button>
-        </Form.Group>
-      </Row>
+      <Row>
+        <Row className="m-4">
+          <Form.Group as={Col}>
+            <Form.Control
+              type="number"
+              placeholder="min price"
+              value={minPrice}
+              onChange={(e) => setMinPrice(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Control
+              type="number"
+              placeholder="max price"
+              value={maxPrice}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Control
+              type="number"
+              placeholder="rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Form.Control
+              type="text"
+              placeholder="subject"
+              value={subject}
+              onChange={(e) => setSubject(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group as={Col}>
+            <Button variant="dark" className="me-4" onClick={filter}>
+              Filter
+            </Button>
+            <Button variant="dark" onClick={clearFilters}>
+              Clear filters
+            </Button>
+          </Form.Group>
+        </Row>
 
-      <div className="myCourses__content">
-        <div className="myCourses__header">
-          <p className="header">My Courses</p>
+        <div className="myCourses__content">
+          <div className="myCourses__header">
+            <p className="header">My Courses</p>
+          </div>
+          <div className="wrapper">
+            {filteredCourses.map((course) => (
+              <CourseCard course={course} />
+            ))}
+          </div>
         </div>
-        <div className="wrapper">
-          {filteredCourses.map((course) => (
-            <CourseCard course={course} />
-          ))}
-        </div>
-      </div>
-    </Row>
+      </Row>
+    </>
   );
 };
 
