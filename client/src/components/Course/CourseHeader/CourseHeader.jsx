5395263c88a38e3a2d80f7cbf4f8735ce3df8416
Ziad Othman <68449722+ziadsadek999@users.noreply.github.com>
2022-12-28 19:06:52 +0200

    instructor name link

--- client/src/components/Course/CourseHeader/CourseHeader.jsx
+++ client/src/components/Course/CourseHeader/CourseHeader.jsx
@@ -56,6 +56,7 @@ function CourseHeader(props) {
                     vc === ViewerContexts.enrolledTrainee
                   }/${instructorId}`
             }
+            className="whiteTxt"
           >
             {instructorName ?? "Instructor"}
           </a>
