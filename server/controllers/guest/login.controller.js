a845d274157c138747deead8ae19471f74af023f
abd0123 <abdof1723@gmail.com>
2022-12-29 22:30:11 +0200

    add corporate name in sessions

--- server/controllers/guest/login.controller.js
+++ server/controllers/guest/login.controller.js
@@ -16,12 +16,14 @@ exports.login = asyncHandler(async (req, res) => {
     req.session.userType = user.userType ?? "admin";
     req.session.userName =
       (user.firstName ?? user.username) + " " + (user.lastName ?? "");
+    req.session.corporateName = user.corporateName;
     res.status(201).json({
       userType: user.userType ?? "admin",
       userId: user._id,
       firstName: user.firstName,
       lastName: user.lastName,
       userName: user.username,
+      corporateName: user.corporateName,
     });
   } else {
     res.status(500).json({ error: "Incorrect username or password." });
